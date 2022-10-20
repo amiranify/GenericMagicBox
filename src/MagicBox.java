41462c814cb0ed95127841c88272ebd3681ce7be
amiranify <amiranify@mail.ru>
2022-10-20 16:21:52 +0300

    first commit and adding a method T pick

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MagicBox<T> {
     protected T[] items;
     protected int size;
@@ -10,16 +12,30 @@ public class MagicBox<T> {
 
     public boolean add(T item) {
         for (int i = 0; i < size; i++) {
-            if (items[i] == null){
+            if (items[i] == null) {
                 items[i] = item;
                 System.out.println(item + " добавлен в коробку.");
                 return true;
-            } else {
-                System.out.println("Коробка полная.");
-                return false;
             }
         }
+        System.out.println("Коробка полная.");
         return false;
     }
 
+    public T pick() throws RuntimeException {
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                count++;
+            }
+        }
+
+        if (count != 0) {
+            throw new RuntimeException("Остались незаполненных ячеек " + count + " шт.");
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt(items.length);
+        System.out.println(items[randomInt]);
+        return items[randomInt];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         MagicBox<String> names = new MagicBox<>(5);
-        names.add("Vasya");
+        names.add("cube");
+        names.add("sphere");
+        names.add("pyramid");
+        names.add("cylinder");
+        names.add("cone");
+
+        System.out.println(names.pick());
+
+        MagicBox<Integer> volume = new MagicBox<>(5);
+        volume.add(10);
+        volume.add(4);
+        volume.add(15);
+        volume.add(30);
+        volume.add(55);
+
+        System.out.println(volume.pick());
+
+
     }
 }
